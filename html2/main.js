c6c69d3b5f6668c8638beca779cdc4c7c761eb3d
ASUS <citozibrand@gmail.com>
2021-06-12 15:24:51 +0200

    PHP getCustomObjects

---
+++ html2/main.js
@@ -0,0 +1,105 @@
+// name = 'silvi';
+// console.log(name);
+// var name;
+
+// function fullName() {
+//     console.log("silvi lila");
+// }
+//
+// fullName();
+
+var name = "Brand";
+let lastName = "Citozi";
+const fatherName = "Agron";
+const description = `Hello World my name is ${name}`;
+//
+// function fullName() {
+//     consol.log(name, lastName);
+// }
+// fullName();
+
+// const getFullName = (...args) => {
+//     console.log(args);
+// }
+
+// const getFullName = (name, lastName, age) => {
+//     console.log(`${name} ${lastName} ${age}`);
+//
+// }
+//
+// getFullName("Brand", "Citozi", "20", "Tirane");
+
+
+// const getDescription = () => {
+//     console.log(description);
+// }
+// getDescription();
+
+//
+// for (let i = 0; i <= 100; i++) {
+//     console.log(description)
+// }
+//
+// //loops
+//
+// for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+// }
+// //
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//
+// const newArray = array.filter(c => c > 5);
+// console.log(newArray.indexOf(8))
+
+// const obj = {
+//     name: "SIlvi",
+//     lastName: "Lila"
+// };
+//
+// console.log(obj['name']);
+
+//
+// const objOrders = {
+//     order_1: {
+//         product: "Bluze",
+//         size: "L",
+//         color: "Red"
+//     },
+//     order_2: {
+//         product: "Bluze",
+//         size: "L",
+//         color: "Red"
+//     },
+//     order_3: {
+//         product: "Bluze",
+//         size: "L",
+//         color: "Red"
+//     },
+// };
+//
+// const getOrder = (order_id) => {
+//     console.log(objOrders[order_id])
+// }
+//
+// getOrder("order_1")
+
+const jsonValues = {
+    "name": "Brand",
+    "lastName": "Citozi"
+}
+
+function CustomObject() {
+    this.name = "Brand";
+    this.lastName = "Citozi";
+
+    this.getFullName = function () {
+        return `${this.name} ${this.lastName}`;
+    }
+}
+
+CustomObject.prototype.fullName = function () {
+    console.log("Brand Citozi");
+}
+
+const custom = new CustomObject();
+const custom1 = new CustomObject();
--- project2/project2/api/index.php
+++ project2/project2/api/index.php
@@ -10,30 +10,36 @@ require_once "../includes/sessionStorage.php";
 $storage = new SessionStorage("orders");
 $requestUri = (parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-switch($requestUri){
-    case "/api/orders":
-        if($_SERVER["REQUEST_METHOD"] === "GET"){
-            if(isset($_GET['order_id'])){
-                echo json_encode($storage->get($_GET['order_id']));
-                return;
+//try {
+//    $headers = getallheaders();
+//    if (isset($headers['x-auth']) && $headers['x-auth'] !== 'silvi') {
+//        throw new Exception("Ja ke fut kot");
+//    }
+    switch ($requestUri) {
+        case "/api/orders":
+            if ($_SERVER["REQUEST_METHOD"] === "GET") {
+                if (isset($_GET['order_id'])) {
+                    echo json_encode($storage->get($_GET['order_id']));
+                    return;
+                }
+
+                echo json_encode($storage->getAll());
             }
 
-            echo json_encode( $storage->getAll());
-        }
-
-        if($_SERVER["REQUEST_METHOD"] === "POST"){
-            $postBody = file_get_contents('php://input');
-            $parsedBody = json_decode($postBody, true);
-            $orderName = "order_".uniqid();
-            $storage->set($orderName, $parsedBody);
-            if($storage->exists($orderName)){
-                echo json_encode("Success");
-                break;
+            if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                $postBody = file_get_contents('php://input');
+                $parsedBody = json_decode($postBody, true);
+                $orderName = "order_" . uniqid();
+                $storage->set($orderName, $parsedBody);
+                if ($storage->exists($orderName)) {
+                    echo json_encode("Success");
+                    break;
+                }
             }
-        }
-        break;
-    default:
-        // echo json_encode("Nuk gjendet route");
-        break;
-}
+            break;
+        default:
+            echo json_encode("Nuk gjendet route");
+            break;
+    }
+
 
